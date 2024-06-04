a6168f0a63e36cb2f53ddbc04af5a38cad6b469f
NickiSamantha <nickisabels73@gmail.com>
2024-06-04 13:31:15 +0200

    alldone

---
+++ script/code.js
@@ -0,0 +1,6 @@
+function calculateArea () {
+    let base = parseFloat(document.getElementById('base').value);
+    let height = parseFloat(document.getElementById('height').value);
+    let area = 0.5 * base * height;
+    document.getElementById('result').innerHTML = "Area:" + area;
+}
